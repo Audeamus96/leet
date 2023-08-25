79fdb196a37afa9e927faddc11e06b4ba8c771c1
Josh M <joshua0833@hotmail.com>
2023-08-24 17:27:20 -0700

    build changes

--- dp/1-fib_dp.cpp
+++ dp/1-fib_dp.cpp
@@ -1,7 +1,6 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "..\utils\catch.hpp"
-#include <vector>
 
+#include <vector>
 
 int fib(int num){
     std::vector<int> memo (num);
