acd63626e680d33c0e76d1b0ef0ff64a0fb4cd32
Josh M <joshua0833@hotmail.com>
2023-09-12 13:00:49 -0700

    Better solution implementation

--- solutions/dp_max_subarray.cpp
+++ solutions/dp_max_subarray.cpp
@@ -3,14 +3,18 @@
 #include <iostream>
 #include <algorithm>
 
+#define isLessThanOne(x) (x < 1)
+
 // Leetcode 53: Maximum Subarray
 // Given an integer array nums, find the subarray with the largest sum, and return its sum.
 // Useful Constraints:
-//      1 <= nums.length <= 105
+//      1 <= nums.length <= 10^5
+//      -10^4 <= nums[i] <= 10^4
 
 
 // First Attempt, O(N) solution
-int maxSubArray(std::vector<int>& nums){
+// This is not a dp solution and clearly really gross looking
+int maxSubArray_non_dp(std::vector<int>& nums){
     if(nums.size() == 1){
         return nums[0];
     }
@@ -18,8 +22,8 @@ int maxSubArray(std::vector<int>& nums){
     int curr_sum = nums[0];
     int max_sum = curr_sum;
     for(int i=1; i < nums.size(); i++){
-        // printf("curr_sum: %i\n", curr_sum);
-        if(curr_sum < 1 && nums[i] > curr_sum){
+        // if curr_sum is less than one and nums[i] is greater, then replace curr_sum
+        if(isLessThanOne(curr_sum) && nums[i] > curr_sum){
             curr_sum = nums[i];
             if(max_sum < curr_sum)
                 max_sum = curr_sum;
@@ -31,6 +35,7 @@ int maxSubArray(std::vector<int>& nums){
             if(max_sum < curr_sum)
                 max_sum = curr_sum;
             curr_sum = nums[i];
+        // otherwise add nums[i] to curr_sum
         }else{
             curr_sum += nums[i];
             if(max_sum < curr_sum)
@@ -40,15 +45,23 @@ int maxSubArray(std::vector<int>& nums){
     return max_sum;
 }
 
-// int main(){
-//  std::vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4};
-//  std::vector<int> nums1 = {5,4,-1,7,8};
-//  std::vector<int> nums2 = {1};
-//  std::vector<int> nums3 = {-2,-3,-4,-4,-5};
-//  std::vector<int> nums4 = {-2,1};
-//  printf("ans: %i\n", maxSubArray(nums));
-//  printf("ans: %i\n", maxSubArray(nums1));
-//  printf("ans: %i\n", maxSubArray(nums2));
-//  printf("ans: %i\n", maxSubArray(nums3));
-//  printf("ans: %i\n", maxSubArray(nums4));
-//  }
+// To work towards a dp solution we first need to break it into subproblems
+// My idea is to break it into the sub-problem maxSubArray(A[], i) where i is the start index of the array (i < A[].length).
+
+int maxSubArray(std::vector<int>& nums){
+    if(nums.size() == 1)
+        return nums[0];
+    
+    int max = nums[0], running_sum = nums[0];
+    for(int i = 1; i < nums.size(); i++){
+        if(running_sum < 0)
+            // if the running sum is negative, adding nums[i] to it will only make it smaller, set it to nums[i] instead
+            running_sum = nums[i]; 
+        else
+            // the running sum is positive, add nums[i] to it and compare that to the golbal max
+            running_sum = running_sum+nums[i];
+        max = std::max(running_sum, max);
+    }
+    
+    return max;
+}
